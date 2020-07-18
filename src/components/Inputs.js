47615d962bb3c3265911aabf2a663ffb27b9f3ff
Anish Kshatriya <obliviga@Anishs-MacBook-Pro-2.local>
2020-07-18 12:54:11 -0700

    adding api key to env variable. only find distance once both input fields have values. adding .env to gitignore

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -2,48 +2,60 @@ import React from 'react'
 import GooglePlacesAutocomplete, { geocodeByAddress} from 'react-google-places-autocomplete';
 import distance from 'google-distance-matrix';
 
-distance.key('AIzaSyDzRYqFfhTGVn5M6WT-hlWiBwOHwacJfpQ');
+distance.key(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+
+distance.units('imperial');
 
 let points = {
-	"pointA": {
-		"latitude": null,
-		"longitude": null
-	},
-	"pointB": {
-		"latitude": null,
-		"longitude": null
-	}
+	pointA: null,
+  pointB: null
 };
 
+function getDistance() {
+  let origins = [`${points.pointA}`];
+  let destinations = [`${points.pointB}`];
+
+  distance.matrix(origins, destinations, function (err, distances) {
+    if (err) {
+      console.log(err);
+    }
+
+    if (!distances) {
+      console.log('no distances');
+    }
+
+    if (distances.rows[0].elements[0].status === "ZERO_RESULTS") {
+      console.log(`Sorry, Google Maps doesn't have specific geolocation data for this place or address yet: ${distances.origin_addresses[0]}`);
+    }
+
+    if (distances.status === 'OK' ) {
+      console.log(distances.rows[0].elements[0].distance.value);
+    }
+  });
+}
+
 function getPointA(data) {
-  geocodeByAddress(data.description)
-    .then(results => (
-			points.pointA.latitude = results[0].geometry.viewport.Za.i,
-			points.pointA.longitude = results[0].geometry.viewport.Ua.i
-			)
-		).catch(error => console.error(error));
+  geocodeByAddress(data.description).then(value => {
+    points.pointA = data.description;
+
+    if (points.pointB !== null) {
+      getDistance();
+    }
+  }, reason => {
+    console.error(reason); // Error!
+  });
 }
 
 function getPointB(data) {
-  geocodeByAddress(data.description)
-    .then(results => (
-			points.pointB.latitude = results[0].geometry.viewport.Za.i,
-			points.pointB.longitude = results[0].geometry.viewport.Ua.i
-			)
-		).catch(error => console.error(error));
+  geocodeByAddress(data.description).then(value => {
+    points.pointB = data.description;
 
-		console.log(points);
-}
-
-function getDistance() {
-	let origins = ['San Francisco CA'];
-	let destinations = ['New York NY', '41.8337329,-87.7321554'];
-
-	distance.matrix(origins, destinations, function (err, distances) {
-		if (!err) {
-			console.log(distances.rows[0].elements[0].distance.value);
-		}
-})
+    if (points.pointA !== null) {
+      getDistance();
+    }
+  }, reason => {
+    console.error(reason); // Error!
+  });
 }
 
 function Inputs() {
