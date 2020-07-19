c222403abae8b2c68780194cc57642484e74c438
Anish Kshatriya <obliviga@Anishs-MacBook-Pro-2.local>
2020-07-18 22:18:20 -0700

    adding some comments to Inputs

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -6,6 +6,7 @@ distance.key(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 
 distance.units('imperial');
 
+// Defining object to store coordinates both points
 let points = {
 	pointA: null,
   pointB: null
@@ -20,24 +21,27 @@ function getDistance() {
       console.log(err);
     }
 
-    if (!distances) {
-      console.log('no distances');
-    }
-
-    if (distances.rows[0].elements[0].status === "ZERO_RESULTS") {
-      console.log(`Sorry, Google Maps doesn't have specific geolocation data for this place or address yet: ${distances.origin_addresses[0]}`);
-    }
-
     if (distances.status === 'OK' ) {
-      console.log(distances.rows[0].elements[0].distance.value);
+      console.log(distances);
+
+      // If the distance can't be calculated due to the impossibility of driving from Point A to B
+      if (distances.rows[0].elements[0].distance === undefined) {
+        console.log(`Sorry, Google Maps can't figure out how you could drive from ${distances.origin_addresses[0]} to ${distances.destination_addresses[0]}`);
+      } else {
+        // Output the driving distance between the two points
+        console.log(distances.rows[0].elements[0].distance.text);
+      }
     }
   });
 }
 
 function getPointA(data) {
-  geocodeByAddress(data.description).then(value => {
+  // Geocode the address
+  geocodeByAddress(data.description).then(() => {
+    // Retrieve the geocoded data and store it inside local points object
     points.pointA = data.description;
 
+    // If Point B input has a value, then calculate the distance
     if (points.pointB !== null) {
       getDistance();
     }
@@ -47,14 +51,15 @@ function getPointA(data) {
 }
 
 function getPointB(data) {
-  geocodeByAddress(data.description).then(value => {
+  geocodeByAddress(data.description).then(() => {
     points.pointB = data.description;
 
+    // Calculate distance only if Point A has a value
     if (points.pointA !== null) {
       getDistance();
     }
   }, reason => {
-    console.error(reason); // Error!
+    console.error(reason);
   });
 }
 
@@ -62,6 +67,7 @@ function Inputs() {
   return (      
     <div>
       <GooglePlacesAutocomplete
+        // Passing data from selected value to function
         onSelect={(data) => getPointA(data)}
         placeholder="Enter place or address for Point A"
       />
