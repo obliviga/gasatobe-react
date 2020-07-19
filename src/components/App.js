90a9641407ccbf73203f7f699281f65d3841d668
Anish Kshatriya <obliviga@Anishs-MacBook-Pro-2.local>
2020-07-19 00:28:54 -0700

    using airbnb eslint rules. onClick car, gas tank explodes and car drives away.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
 import Inputs from './Inputs';
 import Car from './Car';
@@ -6,10 +5,9 @@ import Car from './Car';
 import 'react-google-places-autocomplete/dist/index.min.css';
 
 function App() {
-  
   return (
     <div>
-      <h1>Gasatobe ⛽<Car /></h1>
+      <h1>Gasatobe <Car /></h1>
       <h2>Find out how much you'll spend in gas travelling from Point A to B!</h2>
       <Inputs />
     </div>
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,10 +1,34 @@
-/* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react';
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+import React, { useState } from 'react';
+
+import '../scss/car.scss';
+import explosion from '../images/explosion.gif';
 
 function Car() {
+  // Initial state
+  const [animate, addClass] = useState('');
+
+  // Add class on click
+  function handleClick(e) {
+    addClass('animate');
+  }
+
+  if (animate) {
+    return (
+      <span>
+        <img src={explosion} className="explosion" role="presentation" />
+        <span role="button" onClick={handleClick} className="car-emoji animate">🚗</span>
+      </span>
+    );
+  }
   return (
-    <span className="car-emoji">🚗</span>
-  )
+    <span>
+      ⛽
+      <span role="button" onClick={handleClick} className="car-emoji">🚗</span>
+    </span>
+  );
 }
 
 export default Car;
