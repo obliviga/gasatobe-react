e385bb269991c0583246494fad6d49055d075b69
Anish Kshatriya <obliviga@Anishs-MacBook-Pro-2.local>
2020-07-22 01:01:57 -0700

    passing selected value to state

--- src/js/components/Years.js
+++ src/js/components/Years.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const Years = () => {
   const [years] = useState([]);
+  const [selectedYear, setState] = useState();
 
   // Iterating through 1999 to the current year
   for (let i = 1999; i < new Date().getFullYear(); i += 1) {
@@ -9,17 +10,17 @@ const Years = () => {
     years.push(i + 1);
   }
 
+  function handleChange(e) {
+    setState(e.target.value);
+  }
+
   return (
-    <div>
-      <label htmlFor="yearSelect">
-        Select a year <br />
-        <select name="year" id="yearSelect">
-          {/* Populate with option elements based on the years state and its index */}
-          {years.map((value, index) => <option key={years[index]}>{value}</option>)}
-        </select>
-      </label>
-    </div>
+    <select aria-label="Select a year" onChange={(e) => handleChange(e)}>
+      {/* Populate with option elements based on the years state and its index */}
+      <option value="">Select a year</option>
+      {years.map((value, index) => <option key={years[index]}>{value}</option>)}
+    </select>
   );
-}
+};
 
 export default Years;
