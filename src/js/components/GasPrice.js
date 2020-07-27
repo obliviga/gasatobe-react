5de3820350d41e23b8fbc48d7a52cd1fc470c10d
Anish Kshatriya <obliviga@gmail.com>
2020-07-27 00:45:39 -0700

    reverting gasPrice input type back to "number", as the decimal button is necessary

--- src/js/components/GasPrice.js
+++ src/js/components/GasPrice.js
@@ -16,7 +16,7 @@ const GasPrice = ({ parentCallback }) => {
   return (
     <label htmlFor="gasPrice">
       Update how much you pay for gas per gallon:&nbsp;
-      <input id="gasPrice" type="tel" pattern="[0-9]*" placeholder="e.g. 2.99" defaultValue={2.99} onChange={handleOnChange} min="0" max="10" step=".01" autoFocus/>
+      <input id="gasPrice" type="number" placeholder="e.g. 2.99" defaultValue={2.99} onChange={handleOnChange} min="0" max="10" step=".01" autoFocus/>
     </label>
   );
 };
