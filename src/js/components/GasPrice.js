1230b8c35638b9e7fb43fe24f10ed880befd3d16
Anish Kshatriya <obliviga@gmail.com>
2020-07-27 02:30:17 -0700

    adding error toast for when user enters a negative number in the gasPrice input field

--- src/js/components/GasPrice.js
+++ src/js/components/GasPrice.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
 import '../../scss/gasPrice.scss';
 
 const GasPrice = ({ parentCallback }) => {
   const [gasPrice, setGasPrice] = useState();
 
   const handleOnChange = (e) => {
-    if (e.target.value > 0) {
+    // If user enters a negative number, show error toast
+    if (Math.sign(e.target.value) === -1) {
+      toast.error('Gas prices can only be positive values 🤓');
+    } else {
       setGasPrice(e.target.value);
       parentCallback(e.target.value);
-    } else {
-      // TODO: Error message
-      console.log('Gas prices can only be positive values lol');
     }
   };
 
   return (
     <label htmlFor="gasPrice">
       <span>Update how much you pay for gas per gallon:&nbsp;</span>
-      <input id="gasPrice" type="number" placeholder="e.g. 2.99" defaultValue={2.99} onChange={handleOnChange} min="0" max="10" step=".01" autoFocus/>
+      <input id="gasPrice" type="number" placeholder="e.g. 2.99" defaultValue={2.99} onChange={handleOnChange} min="0" max="10" step=".01" autoFocus />
     </label>
   );
 };
