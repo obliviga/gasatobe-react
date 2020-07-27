3e711d41279e784d91b087fca25f0c896c5df382
Anish Kshatriya <obliviga@gmail.com>
2020-07-26 17:16:59 -0700

    updating gas price message. changing gasPrice input placeholder, adding defaultValue of 2.99 , and adding min, max, and step values of 0, 10, and .01 respectively

--- src/js/components/GasPrice.js
+++ src/js/components/GasPrice.js
@@ -15,8 +15,8 @@ const GasPrice = ({ parentCallback }) => {
 
   return (
     <label htmlFor="gasPrice">
-      Enter how much you pay for gas per gallon&nbsp;
-      <input id="gasPrice" type="number" placeholder="2.99" onChange={handleOnChange} />
+      Update how much you pay for gas per gallon:&nbsp;
+      <input id="gasPrice" type="number" placeholder="e.g. 2.99" defaultValue={2.99} onChange={handleOnChange} min="0" max="10" step=".01" />
     </label>
   );
 };
